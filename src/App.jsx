127ae75e1eca5c1f983442b9f0061b30ccb3e1d1
Samarth Dhoble <samarthdhoble2005@gmail.com>
2025-06-23 22:55:40 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Lottery from './Lottery'
+
+function App() {
+  return (
+    <>
+    <Lottery/>
+    </>
+  )
+}
+
+export default App
